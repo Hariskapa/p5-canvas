e6f9fb2260fb0678f6bc6104b2e8fe4f08e0d991
Hariskapa <xarhs_k@hotmail.com>
2018-12-03 20:09:38 +0200

    Primitives Shapes - rect

---
+++ module08/ex825.js
@@ -0,0 +1,16 @@
+let xPos;
+function setup() {
+  // create canvas
+  createCanvas(800,500);
+  // set background color
+  background('orange');
+}
+function draw () {
+  // use a for loop and create points with a distance of 30px
+  for (xPos = 1; xPos<width; xPos = xPos +30) {
+    // in order to create a diagonal line of points going from up left to down right
+    // you will have to use the appropiate geometry to define the y coordinate
+    // ellipse (xPos,5/8*xPos,6);
+    point (xPos,5/8*xPos);
+  }
+}
